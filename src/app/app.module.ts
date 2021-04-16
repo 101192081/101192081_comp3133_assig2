bb6a53e749eb5d047a55d944457bf80b61cc5e8a
jonathan clarke <jonathan.clarke2@georgebrown.ca>
2021-04-16 12:07:17 -0400

    creating assignment2

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    HotelListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
